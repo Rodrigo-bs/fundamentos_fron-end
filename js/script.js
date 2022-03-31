613716949bf886b81ed05526ad07a7ec3cacdf38
unknown <rodrigo.bs.548@gmail.com>
2022-03-30 21:46:17 -0300

    AJAX

--- js/script.js
+++ js/script.js
@@ -53,8 +53,43 @@ class RemoveElementByTable {
     }
 }
 
+class GetUsers {
+    constructor(button) {
+        this.button = document.querySelector(button);
+        this.url = 'https://raw.githubusercontent.com/danielnsilva/webacademyufac/main/usuarios.json';
+    }
+
+    makeAjax() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', this.url);
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+            }
+        });
+        
+        xhr.send();
+    }
+
+    addEvent() {
+        this.button.addEventListener('click', this.makeAjax);
+    }
+
+    bindEvent() {
+        this.makeAjax = this.makeAjax.bind(this);
+    }
+
+    init() {
+        this.bindEvent();
+        this.addEvent();
+    }
+}
+
 const menu = new Menu('.dropdown > a', '.dropdown-content');
 menu.init();
 
 const removeElementByTable = new RemoveElementByTable('[data-table="button_delete"]', '[data-table="table"]');
 removeElementByTable.init();
+
+const getUsers = new GetUsers('.button-ajax', '.');
+getUsers.init();
